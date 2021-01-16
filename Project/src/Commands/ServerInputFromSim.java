a440349ab21ef9ace9b86069e9f2fbef2b3d23b9
Hen Baliti <58342958+HenBaliti@users.noreply.github.com>
2021-01-16 19:23:00 +0200

    Connecting the simulator client to our server need to fix the binding between the params we get from the simulator client and the params we havwe in the symboltable.

---
+++ Project/src/Commands/ServerInputFromSim.java
@@ -0,0 +1,44 @@
+package Commands;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.List;
+
+import Expression.ShuntingYard;
+import Interpeter.SymbolTabelObject;
+import server_side.ClientHandler;
+import server_side.MyClientHandler;
+import server_side.MySerialServer;
+import server_side.Server;
+
+//open the server
+public class ServerInputFromSim{
+
+	public static void main(String[] args) {
+		
+		MyServerGet ms = new MyServerGet();
+		
+		new Thread(()->{
+		try {
+			ms.runServer();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}).start();
+		
+		}
+	
+	
+	
+}
+
+
